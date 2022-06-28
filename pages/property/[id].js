296c9cefb81fcc5a24e4d88974cf0c99d25fba33
MohamedDiopGit <diopm.contact@gmail.com>
2022-06-28 18:22:26 +0200

    fixing issues

--- pages/property/[id].js
+++ pages/property/[id].js
@@ -1,4 +1,4 @@
-import {Box, Flex, Spacer, Text} from '@chakra-ui/reac';
+import {Box, Flex, Spacer, Text} from '@chakra-ui/react';
 import { FaBed, FaBath } from 'react-icons/fa';
 import { BsGridFill } from 'react-icons/bs';
 import { GoVerified } from 'react-icons/go';
