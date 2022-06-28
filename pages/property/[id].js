eda3ead4e5205ab4f8a6d2e67256a0e1a1c0a9e5
MohamedDiopGit <diopm.contact@gmail.com>
2022-06-28 18:17:37 +0200

    Loading bar and fixing some issues

--- pages/property/[id].js
+++ pages/property/[id].js
@@ -1,1 +1,22 @@
 import {Box, Flex, Spacer, Text} from '@chakra-ui/reac';
+import { FaBed, FaBath } from 'react-icons/fa';
+import { BsGridFill } from 'react-icons/bs';
+import { GoVerified } from 'react-icons/go';
+import millify from 'millify';
+
+import { baseUrl, fetchApi } from '../../utils/fetchApi';
+
+
+const PropertyDetails = ({ propertyDetails: { price, rentFrequency, rooms, title, baths, area, agency, isVerified, description, type, purpose, furnishingStatus, amenities, photos } }) => (
+    <Box maxWidth='1000px' margin='auto' p='4'>
+    </Box>
+)
+
+export async function getServerSideProps({params: {id}}) {
+    const data = fetchApi(`${baseUrl}/properties/detail?externalID=${id}`);
+    return {
+        props: {
+            properyDetails : data
+        }
+    }
+}
